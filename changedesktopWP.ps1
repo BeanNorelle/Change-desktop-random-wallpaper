4c2225973862ab4de285ed9a6a43106a7a515330
Bean <57082197+BeanNorelle@users.noreply.github.com>
2021-07-01 07:57:14 +0800

    Update changedesktopWP.ps1

--- changedesktopWP.ps1
+++ changedesktopWP.ps1
@@ -1,9 +1,10 @@
-Function Set-WallPaper($Value)
+Function Set-WallPaper($Value)//this shit is very buggy needs to be refactored
 {
     Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
     rundll32.exe user32.dll, UpdatePerUserSystemParameters
     $Files = Get-ChildItem -Path C:\Users\beans\OneDrive\wallpaper\*.jpg -Recurse
      $Sample = $Files | Get-Random
+    
      echo "$sample"
 }
 
